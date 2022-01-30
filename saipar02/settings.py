8f73cf58496810e6021eab1082002117d2ed3a89
bk13565 <bryann.kamau@gmail.com>
2022-01-29 23:35:33 -0500

    updated settings for deployment

--- saipar02/settings.py
+++ saipar02/settings.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'saipar02.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 POSTGRES_DB = os.environ.get("POSTGRES_DB") #database name
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD") #database password
