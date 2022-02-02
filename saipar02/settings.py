1f567c9c64969c722884696b6b89d78771e938f3
bk13565 <bryann.kamau@gmail.com>
2022-02-02 06:04:41 -0500

    added saipar_files

--- saipar02/settings.py
+++ saipar02/settings.py
@@ -332,7 +332,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
 # files served in development 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),
+                    os.path.join(BASE_DIR, "saipar_files"),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
