e6d69c00b0c7ddd1e726093d721ac2880522cc93
bk13565 <bryann.kamau@gmail.com>
2022-02-02 16:45:19 -0500

    modified static files settings

--- saipar02/settings.py
+++ saipar02/settings.py
@@ -328,11 +328,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = 'static_root'
 
-
-# files served in development 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "saipar_files"),]
+# directories with static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),
+                    os.path.join(BASE_DIR, "saipar_files"),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
